071b575e499cc51ea45832792fc6edb76f481a6d
Raza Lamb <ec2-user@ip-172-31-49-174.ec2.internal>
2021-09-02 00:12:17 +0000

    made code more efficient

--- main.py
+++ main.py
@@ -12,42 +12,26 @@ async def root():
 @app.get("/generate/{num}")
 async def generate(num: int):
     """Generate a random password of length num"""
-    password = ""
-    characters = '.?!@$%&*_^#'
-    count = {'letter':0, 'character':0, 'number':0}
-    translation = {'letter': string.ascii_letters, 'character': characters, 'number': string.digits}
-    if num < 6 or num > 50:
-        num = 8
-    for i in range(num):
-        integer = random.randint(1,3)
-        if integer == 1:
-            password += random.choice(string.ascii_letters)
-            count['letter'] += 1
-            pass
-        elif integer == 2:
-            password += random.choice(characters)
-            count['character'] += 1
-            pass
-        else:
-            password += random.choice(string.digits)
-            count['number'] += 1
-            pass
+    if num < 3:
+        num = 3
         pass
-    for key, val in count.items():
-        if val == 0:
-            rep = 0
-            for key2, val2 in count:
-                if key2 != key & val2 > rep:
-                    rep = val2
-                    use = key2
-                    pass
-                pass
-            for i in range(num):
-                if password[i] in translation[use]:
-                    password[:i] + random.choice(translation[key]) + password[i + 1:]
-                    break
-                pass
-            pass
+    if num > 50:
+        num = 50
+        pass
+    password = ""
+    count = num // 3
+    remainder = num % 3
+    characters = '.?!@$%&*_^#'
+    len_let = count + remainder - 1
+    password += random.choice(string.ascii_uppercase)
+    for i in range(len_let):
+        password += random.choice(string.ascii_letters)
+        pass
+    for i in range(count):
+        password += random.choice(characters)
+        password += random.choice(string.digits)
+        pass
+    password = ''.join(random.sample(password, len(password)))
     return {"password": password}
 
 if __name__ == '__main__':
